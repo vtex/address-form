a99eab4f697aa56c1372611666bfd8bcfca2cca6
Fernando de Almeida Coelho <fernandocoelho@Fernandos-MacBook-Pro.local>
2017-06-07 18:04:54 -0300

    Initial commit

--- src/AddressForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import AddressForm from "./AddressForm"
-
-it("renders AddressForm", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AddressForm />, div);
-});
