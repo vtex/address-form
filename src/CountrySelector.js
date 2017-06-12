f01938fad5ec6acde420586e7c276cca11f9e815
Breno Calazans <brenocalazans@gmail.com>
2017-06-12 18:38:15 -0300

    Implement CountrySelector

--- src/CountrySelector.js
+++ src/CountrySelector.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import CountryType from './propTypes/CountryType'
 
 class CountrySelector extends Component {
   render() {
-    return <div />
+    const { country, shipsTo, onChangeSelectedCountry } = this.props
+
+    return (
+      <div className="country-selector">
+        <label>
+          País
+          <select value={country} onChange={onChangeSelectedCountry}>
+            {shipsTo.map(country => (
+              <option value={country} key={country}>
+                {country}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+    )
   }
 }
 
 CountrySelector.propTypes = {
+  country: CountryType,
   shipsTo: PropTypes.array.isRequired,
   onChangeSelectedCountry: PropTypes.func.isRequired,
 }
--- src/CountrySelector.test.js
+++ src/CountrySelector.test.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import CountrySelector from './CountrySelector'
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const wrapper = shallow(
-    <CountrySelector shipsTo={['BRA']} onChangeSelectedCountry={jest.fn()} />
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <CountrySelector
+      selected={'BRA'}
+      shipsTo={['BRA', 'USA']}
+      onChangeSelectedCountry={jest.fn()}
+    />,
+    div
   )
-  const div = <div />
-  expect(wrapper.contains(div)).toEqual(true)
+})
+
+it('show options', () => {
+  const tree = renderer
+    .create(
+      <CountrySelector
+        shipsTo={['BRA', 'USA']}
+        onChangeSelectedCountry={jest.fn()}
+      />
+    )
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
+
+it('default value', () => {
+  const tree = renderer
+    .create(
+      <CountrySelector
+        country={'BRA'}
+        shipsTo={['BRA', 'USA']}
+        onChangeSelectedCountry={jest.fn()}
+      />
+    )
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
+
+it('react to change value', () => {
+  const handleChange = jest.fn()
+  const wrapper = shallow(
+    <CountrySelector
+      country={'BRA'}
+      shipsTo={['BRA', 'USA']}
+      onChangeSelectedCountry={handleChange}
+    />
+  )
+
+  const event = { target: { value: 'USA' } }
+  wrapper.find('select').simulate('change', event)
+
+  expect(handleChange).toHaveBeenCalledWith(event)
 })
