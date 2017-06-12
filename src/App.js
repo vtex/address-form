4a99bb824dc78184f43ee3c129b2f3477f8d3ae5
Breno Calazans <brenocalazans@gmail.com>
2017-06-12 16:14:39 -0300

    Init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import style from './App.css'
+import 'vtex-tachyons'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
+        <img src={logo} className={style.appLogo} alt="logo" />
+        <h2>Welcome to VTEX</h2>
+        <p className={style.appIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
