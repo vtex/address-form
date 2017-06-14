5da4515715b142de4e6c5c8898958f7510120a66
Breno Calazans <brenocalazans@gmail.com>
2017-06-14 14:51:57 -0300

    Fix propTypes

--- src/AddressSummary.js
+++ src/AddressSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import AddressShape from './AddressShape'
+import AddressShape from './propTypes/AddressShape'
 
 class AddressSummary extends Component {
   render() {
