77a49442fe35784d08fe0794d9b5971c19ef8ef3
Breno Calazans <brenocalazans@gmail.com>
2017-06-30 15:00:37 -0300

    Add missing tests

--- src/transforms/address.test.js
+++ src/transforms/address.test.js
@@ -1,4 +1,4 @@
-import { addValidation, removeValidation } from './address'
+import { addValidation, removeValidation, addNewField } from './address'
 import address from '../__mocks__/newAddress'
 import addressWithoutValidation from '../__mocks__/addressWithoutValidation'
 
@@ -13,5 +13,30 @@ describe('Address Transform', () => {
     const result = removeValidation(address)
 
     expect(result).toMatchObject(addressWithoutValidation)
+  })
+
+  it('should add a new field to all address properties', () => {
+    const fieldName = 'foo'
+    const value = 'bar'
+
+    const result = addNewField(address, fieldName, value)
+
+    expect(result).toHaveProperty(`addressId.${fieldName}`, value)
+    expect(result).toHaveProperty(`postalCode.${fieldName}`, value)
+  })
+
+  it('should add a new field property but keep previous ones', () => {
+    const changedAddress = {
+      ...address,
+      postalCode: { value: '123', valid: true },
+    }
+    const fieldName = 'foo'
+    const value = 'bar'
+
+    const result = addNewField(changedAddress, fieldName, value)
+
+    expect(result).toHaveProperty(`postalCode.${fieldName}`, value)
+    expect(result).toHaveProperty('postalCode.value', '123')
+    expect(result).toHaveProperty('postalCode.valid', true)
   })
 })
