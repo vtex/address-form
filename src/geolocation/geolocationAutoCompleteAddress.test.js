1ed24981235708ac5b5640d42222238b9440c332
Breno Calazans <brenocalazans@gmail.com>
2017-07-18 19:44:35 -0300

    Add invalid field test case for geolocation

--- src/geolocation/geolocationAutoCompleteAddress.test.js
+++ src/geolocation/geolocationAutoCompleteAddress.test.js
@@ -1,6 +1,7 @@
 import geolocationAutoCompleteAddress from './geolocationAutoCompleteAddress'
 import postalCodeGoogleAddress from './__mocks__/postalCodeGoogleAddress'
 import oneLevelGoogleAddress from './__mocks__/oneLevelGoogleAddress'
+import invalidFieldGoogleAddress from './__mocks__/invalidFieldGoogleAddress'
 import usePostalCode from '../country/__mocks__/usePostalCode'
 import useOneLevel from '../country/__mocks__/useOneLevel'
 
@@ -23,5 +24,16 @@ describe('Geolocation Auto Complete Address', () => {
     )
 
     expect(address.postalCode.value).toBe('0000')
+  })
+
+  it('should get an address with a focus on the first invalid field', () => {
+    const address = geolocationAutoCompleteAddress(
+      invalidFieldGoogleAddress,
+      usePostalCode,
+      usePostalCode.country
+    )
+
+    expect(address.postalCode.valid).toBe(false)
+    expect(address.postalCode.focus).toBe(true)
   })
 })
