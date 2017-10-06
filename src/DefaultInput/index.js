5af26a83033267f2b3c648f97c8ff822c7fcb0be
Breno Calazans <brenocalazans@gmail.com>
2017-10-06 15:10:56 -0300

    Disable input while postal code is loading

--- src/DefaultInput/index.js
+++ src/DefaultInput/index.js
@@ -24,6 +24,7 @@ class Input extends Component {
             address={address}
             onChange={this.props.onChange}
             onBlur={this.props.onBlur}
+            disabled={loading}
             inputRef={inputRef}
           />
           {loading && <PostalCodeLoader />}
