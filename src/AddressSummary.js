7e121bd2fd692193539573da4f3fc370d880898a
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-18 12:02:27 -0300

    Use defaultMessage prop

--- src/AddressSummary.js
+++ src/AddressSummary.js
@@ -77,7 +77,8 @@ class AddressSummary extends Component {
           <span key="country" className="country">
             {this.props.intl.formatMessage({
               id: `country.${rules.country}`,
-            }) || rules.country}
+              defaultMessage: rules.country,
+            })}
           </span>,
         ]}
         {children}
