c262d0d0dc8594e8bc8fae9221d190c28ddc60c6
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-19 11:50:44 -0300

    Create intl component

---
+++ src/AddressIntl.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class AddressIntl extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    const {} = this.props
+    return <h1> AddressIntl works! </h1>
+  }
+}
+
+AddressIntl.propTypes = {}
+
+export default AddressIntl
