f0271c658362410dd804dd3afc2fd68a90548a63
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-19 18:02:36 -0300

    Fix enzyme config to match new version

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
 import 'jest-enzyme'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 global.console.warn = warn => {
   throw new Error(warn)
