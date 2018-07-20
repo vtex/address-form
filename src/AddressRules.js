130876e187f8caada5e20d7c2e7dc51f411a1086
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-20 11:28:25 -0300

    Hotfix regex

--- src/AddressRules.js
+++ src/AddressRules.js
@@ -48,7 +48,7 @@ class AddressRules extends Component {
   }
 
   parseError(e) {
-    const regex = new RegExp(/Cannot find module '\.\/([A-z-]{1,7})\'\./)
+    const regex = new RegExp(/Cannot find module '\.\/([A-z-]{1,7})'\./)
     const result = regex.exec(e.message)
     if (!result) return false
     return result[1]
