7e28f9d5a2396e9ddc6e2ad20fea67a858e5a71f
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-20 10:29:52 -0300

    Keep backwards compatibility

--- src/addressRulesContext.js
+++ src/addressRulesContext.js
@@ -4,6 +4,9 @@ export const RulesContext = React.createContext()
 
 export function injectRules(Component) {
   return function InjectedComponent(props) {
+    // eslint-disable-next-line react/prop-types
+    if (props.rules) return <Component {...props} />
+
     return (
       <RulesContext.Consumer>
         {rules => <Component {...props} rules={rules} />}
