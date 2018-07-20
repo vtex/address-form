42a14bb5491ad5bd64028b9dd81e23d9936614d4
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-20 11:24:05 -0300

    Update demo

--- demo/src/Container.js
+++ demo/src/Container.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import IntlContainer from './IntlContainer'
-import Styles from './Styles'
 import App from './App'
-import IntlApp from './IntlApp'
-import { IntlProvider } from 'react-intl'
-import AddressIntl from '../../src/AddressIntl'
-import RulesApp from './RulesApp'
 
 const ACCOUNT_NAME = 'qamarketplace'
 const API_KEY = 'AIzaSyATLp76vkHxfMZqJF_sJbjQqZwvSIBhsTM'
+const locale = 'en'
 const shipsTo = [
   'ARG',
   'BOL',
@@ -29,72 +25,16 @@ const shipsTo = [
 ]
 
 class Container extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      locale: 'en',
-    }
-  }
-
-  handleClick = () => {
-    if (this.state.locale === 'en') {
-      this.setState({
-        locale: 'pt-BR',
-      })
-    } else {
-      this.setState({
-        locale: 'en',
-      })
-    }
-  }
-
   render() {
-    // return (
-    //   <IntlContainer locale={locale}>
-    //     <Styles>
-    //       <App
-    //         accountName={ACCOUNT_NAME}
-    //         googleMapsAPIKey={API_KEY}
-    //         locale={locale}
-    //         shipsTo={shipsTo}
-    //       />
-    //     </Styles>
-    //   </IntlContainer>
-    // )
-
-    const { locale } = this.state
-
     return (
-      <IntlProvider locale={'en'} messages={null}>
-        <div>
-          <AddressIntl
-            locale={locale}
-            imports={(locale, baseLocale) => {
-              return {
-                countryData: import('i18n-iso-countries/langs/' +
-                  baseLocale +
-                  '.json'),
-                reactData: import('react-intl/locale-data/' + baseLocale),
-                addressBaseData: import('../../src/locales/' +
-                  baseLocale +
-                  '.json'),
-                addressData: import('../../src/locales/' + locale + '.json'),
-              }
-            }}
-          >
-            <div>
-              <h3>AddressIntl demo:</h3>
-              <button className="mv6" onClick={this.handleClick}>
-                Switch locale to {locale == 'pt-BR' ? 'en' : 'pt-BR'}
-              </button>
-              <IntlApp shipsTo={shipsTo} />
-              <hr className="mv8" />
-              <h3>AddressRules demo:</h3>
-              <RulesApp />
-            </div>
-          </AddressIntl>
-        </div>
-      </IntlProvider>
+      <IntlContainer locale={locale}>
+        <App
+          accountName={ACCOUNT_NAME}
+          googleMapsAPIKey={API_KEY}
+          locale={locale}
+          shipsTo={shipsTo}
+        />
+      </IntlContainer>
     )
   }
 }
