b5a3007df91122db4cfad5e6236e68dd51abd6c6
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-26 14:03:48 -0300

    Fixed tests

--- src/__mocks__/facebookAddress.js
+++ src/__mocks__/facebookAddress.js
@@ -2,9 +2,11 @@ import address from './addressWithoutValidation'
 
 export default {
   ...address,
+  postalCode: '94025',
   street: '1 Hacker Way',
   complement: '2nd floor',
   city: 'Menlo Park',
   state: 'CA',
   country: 'USA',
+  receiverName: 'Steve Jobs',
 }
