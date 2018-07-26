9771806dc1b3016246b4afab8f81940f6b3cc86d
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-26 16:07:55 -0300

    Uncomment tests and use xit

--- src/AddressContainer.test.js
+++ src/AddressContainer.test.js
@@ -5,6 +5,8 @@ import address from './__mocks__/newAddress'
 import usePostalCode from './country/__mocks__/usePostalCode'
 import postalCodeAutoCompleteAddress from './postalCodeAutoCompleteAddress'
 
+/* #FIXME when enzyme adds suport for context */
+
 jest.mock('./postalCodeAutoCompleteAddress')
 
 describe('AddressContainer', () => {
@@ -15,65 +17,65 @@ describe('AddressContainer', () => {
     country: { value: 'BRA' },
   }
 
-  it('should render its children', () => {
-    // const wrapper = mount(
-    //   <AddressContainer
-    //     cors
-    //     accountName={accountName}
-    //     address={addressWithCountry}
-    //     onChangeAddress={jest.fn()}
-    //     rules={usePostalCode}
-    //   >
-    //     <span className="unique" />
-    //   </AddressContainer>,
-    // )
-    // expect(wrapper.find('.unique')).toHaveLength(1)
+  xit('should render its children', () => {
+    const wrapper = mount(
+      <AddressContainer
+        cors
+        accountName={accountName}
+        address={addressWithCountry}
+        onChangeAddress={jest.fn()}
+        rules={usePostalCode}
+      >
+        <span className="unique" />
+      </AddressContainer>,
+    )
+    expect(wrapper.find('.unique')).toHaveLength(1)
   })
 
-  it('should handle the validation before calling onChangeAddress', () => {
-    // const handleAddressChange = jest.fn()
-    // const children = onChangeAddress => {
-    //   onChangeAddress({ city: { value: 'Rio de Janeiro' } })
-    //   return <span />
-    // }
-    // mount(
-    //   <AddressContainer
-    //     cors
-    //     accountName={accountName}
-    //     address={addressWithCountry}
-    //     onChangeAddress={handleAddressChange}
-    //     rules={usePostalCode}
-    //   >
-    //     {children}
-    //   </AddressContainer>,
-    // )
-    // expect(handleAddressChange).toHaveBeenCalledWith({
-    //   ...addressWithCountry,
-    //   city: { value: 'Rio de Janeiro', valid: true },
-    // })
+  xit('should handle the validation before calling onChangeAddress', () => {
+    const handleAddressChange = jest.fn()
+    const children = onChangeAddress => {
+      onChangeAddress({ city: { value: 'Rio de Janeiro' } })
+      return <span />
+    }
+    mount(
+      <AddressContainer
+        cors
+        accountName={accountName}
+        address={addressWithCountry}
+        onChangeAddress={handleAddressChange}
+        rules={usePostalCode}
+      >
+        {children}
+      </AddressContainer>,
+    )
+    expect(handleAddressChange).toHaveBeenCalledWith({
+      ...addressWithCountry,
+      city: { value: 'Rio de Janeiro', valid: true },
+    })
   })
 
-  it('should short-circuit when country changes', () => {
-    // const handleAddressChange = jest.fn()
-    // const children = jest.fn(onChangeAddress => {
-    //   onChangeAddress({ country: { value: 'ECU' } })
-    //   return <span />
-    // })
-    // mount(
-    //   <AddressContainer
-    //     cors
-    //     accountName={accountName}
-    //     address={addressWithCountry}
-    //     onChangeAddress={handleAddressChange}
-    //     rules={usePostalCode}
-    //   >
-    //     {children}
-    //   </AddressContainer>,
-    // )
-    // expect(handleAddressChange).toHaveBeenCalledWith({
-    //   ...addressWithCountry,
-    //   country: { value: 'ECU' },
-    // })
+  xit('should short-circuit when country changes', () => {
+    const handleAddressChange = jest.fn()
+    const children = jest.fn(onChangeAddress => {
+      onChangeAddress({ country: { value: 'ECU' } })
+      return <span />
+    })
+    mount(
+      <AddressContainer
+        cors
+        accountName={accountName}
+        address={addressWithCountry}
+        onChangeAddress={handleAddressChange}
+        rules={usePostalCode}
+      >
+        {children}
+      </AddressContainer>,
+    )
+    expect(handleAddressChange).toHaveBeenCalledWith({
+      ...addressWithCountry,
+      country: { value: 'ECU' },
+    })
   })
 
   describe('Postal code auto complete', () => {
@@ -81,89 +83,89 @@ describe('AddressContainer', () => {
       postalCodeAutoCompleteAddress.mockClear()
     })
 
-    it('should auto complete postal code when postal code is valid', () => {
-      // const handleAddressChange = jest.fn()
-      // const children = jest.fn(onChangeAddress => {
-      //   onChangeAddress({ postalCode: { value: '22231000' } })
-      //   return <span />
-      // })
-      // mount(
-      //   <AddressContainer
-      //     cors
-      //     accountName={accountName}
-      //     address={addressWithCountry}
-      //     onChangeAddress={handleAddressChange}
-      //     rules={usePostalCode}
-      //   >
-      //     {children}
-      //   </AddressContainer>,
-      // )
-      // expect(postalCodeAutoCompleteAddress).toHaveBeenCalled()
+    xit('should auto complete postal code when postal code is valid', () => {
+      const handleAddressChange = jest.fn()
+      const children = jest.fn(onChangeAddress => {
+        onChangeAddress({ postalCode: { value: '22231000' } })
+        return <span />
+      })
+      mount(
+        <AddressContainer
+          cors
+          accountName={accountName}
+          address={addressWithCountry}
+          onChangeAddress={handleAddressChange}
+          rules={usePostalCode}
+        >
+          {children}
+        </AddressContainer>,
+      )
+      expect(postalCodeAutoCompleteAddress).toHaveBeenCalled()
     })
 
-    it('should not auto complete postal code when postal code is invalid', () => {
-      // const handleAddressChange = jest.fn()
-      // const children = jest.fn(onChangeAddress => {
-      //   onChangeAddress({ postalCode: { value: '222' } })
-      //   return <span />
-      // })
-      // mount(
-      //   <AddressContainer
-      //     cors
-      //     accountName={accountName}
-      //     address={addressWithCountry}
-      //     onChangeAddress={handleAddressChange}
-      //     rules={usePostalCode}
-      //   >
-      //     {children}
-      //   </AddressContainer>,
-      // )
-      // expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
+    xit('should not auto complete postal code when postal code is invalid', () => {
+      const handleAddressChange = jest.fn()
+      const children = jest.fn(onChangeAddress => {
+        onChangeAddress({ postalCode: { value: '222' } })
+        return <span />
+      })
+      mount(
+        <AddressContainer
+          cors
+          accountName={accountName}
+          address={addressWithCountry}
+          onChangeAddress={handleAddressChange}
+          rules={usePostalCode}
+        >
+          {children}
+        </AddressContainer>,
+      )
+      expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
     })
 
-    it('should not auto complete postal code when postal code was auto completed by geolocation', () => {
-      // const handleAddressChange = jest.fn()
-      // const children = jest.fn(onChangeAddress => {
-      //   onChangeAddress({
-      //     postalCode: { value: '22231000', geolocationAutoCompleted: true },
-      //   })
-      //   return <span />
-      // })
-      // mount(
-      //   <AddressContainer
-      //     cors
-      //     accountName={accountName}
-      //     address={addressWithCountry}
-      //     onChangeAddress={handleAddressChange}
-      //     rules={usePostalCode}
-      //   >
-      //     {children}
-      //   </AddressContainer>,
-      // )
-      // expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
+    xit('should not auto complete postal code when postal code was auto completed by geolocation', () => {
+      const handleAddressChange = jest.fn()
+      const children = jest.fn(onChangeAddress => {
+        onChangeAddress({
+          postalCode: { value: '22231000', geolocationAutoCompleted: true },
+        })
+        return <span />
+      })
+      mount(
+        <AddressContainer
+          cors
+          accountName={accountName}
+          address={addressWithCountry}
+          onChangeAddress={handleAddressChange}
+          rules={usePostalCode}
+        >
+          {children}
+        </AddressContainer>,
+      )
+      expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
     })
 
-    it('should not auto complete postal code when prop autoCompletePostalCode is false', () => {
-      //   const handleAddressChange = jest.fn()
-      //   const children = jest.fn(onChangeAddress => {
-      //     onChangeAddress({
-      //       postalCode: { value: '22231000' },
-      //     })
-      //     return <span />
-      //   })
-      //   mount(
-      //     <AddressContainer
-      //       cors
-      //       accountName={accountName}
-      //       address={addressWithCountry}
-      //       onChangeAddress={handleAddressChange}
-      //       autoCompletePostalCode={false}
-      //       rules={usePostalCode}
-      //     >
-      //       {children}
-      //     </AddressContainer>,
-      //   )
-      //   expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
+    xit('should not auto complete postal code when prop autoCompletePostalCode is false', () => {
+      const handleAddressChange = jest.fn()
+      const children = jest.fn(onChangeAddress => {
+        onChangeAddress({
+          postalCode: { value: '22231000' },
+        })
+        return <span />
+      })
+      mount(
+        <AddressContainer
+          cors
+          accountName={accountName}
+          address={addressWithCountry}
+          onChangeAddress={handleAddressChange}
+          autoCompletePostalCode={false}
+          rules={usePostalCode}
+        >
+          {children}
+        </AddressContainer>,
+      )
+      expect(postalCodeAutoCompleteAddress).not.toHaveBeenCalled()
     })
   })
 })
