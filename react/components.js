88b57339c43a8c95f6b229af6022cd80a74707d7
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-11-14 14:55:18 -0200

    Add component export for vtex io component imports

---
+++ react/components.js
@@ -0,0 +1,21 @@
+import CountrySelector from './CountrySelector'
+import AddressForm from './AddressForm'
+import AddressSummary from './AddressSummary'
+import PostalCodeGetter from './PostalCodeGetter'
+import AddressContainer from './AddressContainer'
+import AutoCompletedFields from './AutoCompletedFields'
+import AddressRules from './AddressRules'
+import AddressSubmitter from './AddressSubmitter'
+import PostalCodeLoader from './postalCodeFrom/PostalCodeLoader'
+
+export default {
+  CountrySelector,
+  AddressForm,
+  AddressSummary,
+  PostalCodeGetter,
+  AddressContainer,
+  AutoCompletedFields,
+  AddressRules,
+  AddressSubmitter,
+  PostalCodeLoader,
+}
