6be6e55b4321997d725205010529ddd7d46c19f3
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-11-26 18:50:00 -0200

    Add new components for io imports

--- react/inputs.js
+++ react/inputs.js
@@ -4,9 +4,11 @@ import InputText from './inputs/DefaultInput/InputText'
 import InputSelect from './inputs/DefaultInput/InputSelect'
 import InputLabel from './inputs/DefaultInput/InputLabel'
 import StyleguideInput from './inputs/StyleguideInput'
+import GeolocationInput from './geolocation/GeolocationInput'
 
 export default {
   DefaultInput,
+  GeolocationInput,
   InputError,
   InputLabel,
   InputSelect,
