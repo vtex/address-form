cbb8d5099ce61826d2b434ee3d80da19ae9f54e0
Joao Fidelis <jgfidelis@gmail.com>
2018-11-30 16:17:19 -0200

    fix(country-rules): export more country rules and fix dep of react-intl

--- react/countries.js
+++ react/countries.js
@@ -1,6 +1,46 @@
 // countries
 import defaultRules from './country/default'
+import ARG from './country/ARG'
+import BOL from './country/BOL'
+import BRA from './country/BRA'
+import CAN from './country/CAN'
+import CHL from './country/CHL'
+import COL from './country/COL'
+import ECU from './country/ECU'
+import ESP from './country/ESP'
+import FRA from './country/FRA'
+import GBR from './country/GBR'
+import GTM from './country/GTM'
+import KOR from './country/KOR'
+import MEX from './country/MEX'
+import PER from './country/PER'
+import PRT from './country/PRT'
+import PRY from './country/PRY'
+import ROU from './country/ROU'
+import URY from './country/URY'
+import USA from './country/USA'
+import VEN from './country/VEN'
 
 export default {
   defaultRules,
+  ARG,
+  BOL,
+  BRA,
+  CAN,
+  CHL,
+  COL,
+  ECU,
+  ESP,
+  FRA,
+  GBR,
+  GTM,
+  KOR,
+  MEX,
+  PER,
+  PRT,
+  PRY,
+  ROU,
+  URY,
+  USA,
+  VEN,
 }
