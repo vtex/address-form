b19f1d6a349137a14c139b972e7c6c0da15ec855
nandoacoelho <nandoacoelho@gmail.com>
2019-05-16 15:25:04 -0300

    Add country restrictions when geolocation autocomplete address

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Country restrictions when geolocation autocomplete address
+
 ## [3.5.9] - 2019-05-08
 
 ### Fixed
--- react/geolocation/Utils.js
+++ react/geolocation/Utils.js
@@ -1,41 +1,44 @@
 import geolocationAutoCompleteAddress from './geolocationAutoCompleteAddress'
 
 export default function getAddressByGeolocation(geolocationProps) {
-  const {
-    address,
-    onChangeAddress,
-    rules,
-    googleMaps,
-  } = geolocationProps
+  const { address, onChangeAddress, rules, googleMaps } = geolocationProps
 
   if (!googleMaps || !address || !rules || !address['number'].value) {
     return
   }
 
   const geocoder = new googleMaps.Geocoder()
-  geocoder.geocode({ address: `${address['number'].value} ${address['street'].value}` }, (results, status) => {
-    if (status === googleMaps.GeocoderStatus.OK) {
-      if (results[0]) {
-        const googleAddress = results[0]
-        const autoCompletedAddress = geolocationAutoCompleteAddress(
-          address,
-          googleAddress,
-          rules
-        )
+  geocoder.geocode(
+    {
+      componentRestrictions: {
+        country: rules.abbr,
+      },
+      address: `${address['number'].value} ${address['street'].value}`,
+    },
+    (results, status) => {
+      if (status === googleMaps.GeocoderStatus.OK) {
+        if (results[0]) {
+          const googleAddress = results[0]
+          const autoCompletedAddress = geolocationAutoCompleteAddress(
+            address,
+            googleAddress,
+            rules.geolocation,
+          )
 
-        onChangeAddress({
-          ...autoCompletedAddress,
-          complement: {
-            value: null,
-          },
-          reference: {
-            value: null,
-          },
-        })
-        return autoCompletedAddress
+          onChangeAddress({
+            ...autoCompletedAddress,
+            complement: {
+              value: null,
+            },
+            reference: {
+              value: null,
+            },
+          })
+          return autoCompletedAddress
+        }
+      } else {
+        console.warn(`Google Maps Error: ${status}`)
       }
-    } else {
-      console.warn(`Google Maps Error: ${status}`)
-    }
-  })
+    },
+  )
 }
