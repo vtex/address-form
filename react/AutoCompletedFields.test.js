92aa126797d005a136a41145bee05d1c555ae941
Breno Calazans <brenocalazans@gmail.com>
2019-07-26 11:31:53 -0300

    Update tests

--- react/AutoCompletedFields.test.js
+++ react/AutoCompletedFields.test.js
@@ -29,7 +29,7 @@ describe('AutoCompletedFields', () => {
         {children}
       </AutoCompletedFields>
     )
-    expect(wrapper.html()).toBe(null)
+    expect(wrapper.html()).toBe('')
   })
 
   it('should display nothing if there are autocompleted fields with no value', () => {
@@ -49,7 +49,7 @@ describe('AutoCompletedFields', () => {
         {children}
       </AutoCompletedFields>
     )
-    expect(wrapper.html()).toBe(null)
+    expect(wrapper.html()).toBe('')
   })
 
   describe('', () => {
