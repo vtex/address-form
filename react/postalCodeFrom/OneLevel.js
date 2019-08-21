e0630ca0d8601d90b853690141fffc0f668d1977
nandoacoelho <nandoacoelho@gmail.com>
2019-08-21 11:58:44 -0300

    Update OneLevel to have optional button

--- react/postalCodeFrom/OneLevel.js
+++ react/postalCodeFrom/OneLevel.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import AddressShapeWithValidation from '../propTypes/AddressShapeWithValidation'
 import SelectPostalCode from './SelectPostalCode'
+import SubmitButton from './SubmitButton'
 
 class OneLevel extends Component {
   render() {
-    const { address, rules, onChangeAddress, Input } = this.props
+    const {
+      address,
+      Button,
+      Input,
+      onSubmit,
+      onChangeAddress,
+      rules,
+      submitLabel,
+    } = this.props
 
+    if (Button && onSubmit) {
+      return (
+        <form className="vtex-address-form__oneLevel" onSubmit={onSubmit}>
+          <SelectPostalCode
+            address={address}
+            Input={Input}
+            rules={rules}
+            onChangeAddress={onChangeAddress}
+          />
+          <SubmitButton Button={Button} buttonLabel={submitLabel} />
+        </form>
+      )
+    }
     return (
       <div>
         <SelectPostalCode
@@ -21,10 +43,13 @@ class OneLevel extends Component {
 }
 
 OneLevel.propTypes = {
-  Input: PropTypes.func.isRequired,
   address: AddressShapeWithValidation,
+  Button: PropTypes.func,
+  Input: PropTypes.func.isRequired,
   rules: PropTypes.object.isRequired,
   onChangeAddress: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
+  submitLabel: PropTypes.string,
 }
 
 export default OneLevel
