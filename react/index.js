6c414d136e0d73e8e639a3d388feed34510d5ea4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-09-01 10:40:02 -0300

    Add components and helpers named export

--- react/index.js
+++ react/index.js
@@ -8,8 +8,12 @@ export { default as AutoCompletedFields } from './AutoCompletedFields'
 export { default as AddressRules } from './AddressRules'
 export { default as AddressSubmitter } from './AddressSubmitter'
 
+export { default as components } from './components'
+
 // Helper Functions
 export { addValidation, removeValidation } from './transforms/address'
 export { isValidAddress, validateField } from './validateAddress'
 export { injectRules } from './addressRulesContext'
 export { injectAddressContext } from './addressContainerContext'
+
+export { default as helpers } from './helpers'
