025422828bcec46357ee6290dc05d6b9e4d9a2c8
Arthur Costa <arthurcosta@vtex.com.br>
2021-04-28 12:23:15 -0300

    Remove unwanted .only

--- react/postalCodeAutoCompleteAddress.test.js
+++ react/postalCodeAutoCompleteAddress.test.js
@@ -77,7 +77,7 @@ describe('postalCodeAutoCompleteAddress()', () => {
     })
   })
 
-  it.only('should keep address id when auto completing fields', done => {
+  it('should keep address id when auto completing fields', done => {
     const addressId = 'addressId1autoComplete'
     const modifiedAddress = {
       ...address,
