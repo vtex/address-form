c4635fc1953a1d98257e617ef1e705ddc79303c5
Estacio Pereira <estacio.pereira@vtex.com.br>
2021-07-14 13:57:40 -0300

    Add 'GeolocationInput' to exported 'components'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- `GeolocationInput` to exported `components`.
+
 ## [4.7.5] - 2021-06-22
 
 ### Fixed
--- react/components.js
+++ react/components.js
@@ -8,6 +8,7 @@ import AddressRules from './AddressRules'
 import AddressSubmitter from './AddressSubmitter'
 import PostalCodeLoader from './postalCodeFrom/PostalCodeLoader'
 import GoogleMapsContainer from './geolocation/GoogleMapsContainer'
+import GeolocationInput from './geolocation/GeolocationInput'
 import Map from './geolocation/Map'
 import StyleguideInput from './inputs/StyleguideInput'
 import StyleguideButton from './inputs/StyleguideButton'
@@ -21,6 +22,7 @@ export default {
   AutoCompletedFields,
   CountrySelector,
   GoogleMapsContainer,
+  GeolocationInput,
   Map,
   PostalCodeGetter,
   PostalCodeLoader,
