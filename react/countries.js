99fd5cf1fa2302fe71a7cb298e085a1a325b9a1d
kaio-donadelli <kaio.donadelli@vtex.com.br>
2022-07-21 09:28:36 -0300

    Add missing GTM, CRI, and PAN states and cities

--- react/countries.js
+++ react/countries.js
@@ -7,15 +7,17 @@ import BRA from './country/BRA'
 import CAN from './country/CAN'
 import CHL from './country/CHL'
 import COL from './country/COL'
+import CRI from './country/CRI'
 import ECU from './country/ECU'
 import ESP from './country/ESP'
 import FRA from './country/FRA'
 import GBR from './country/GBR'
 import GTM from './country/GTM'
-import IDN from './country/GTM'
+import IDN from './country/IDN'
 import KOR from './country/KOR'
 import MEX from './country/MEX'
 import NZL from './country/NZL'
+import PAN from './country/PAN'
 import PER from './country/PER'
 import PRT from './country/PRT'
 import PRY from './country/PRY'
@@ -33,6 +35,7 @@ export default {
   CAN,
   CHL,
   COL,
+  CRI,
   ECU,
   ESP,
   FRA,
@@ -42,6 +45,7 @@ export default {
   KOR,
   MEX,
   NZL,
+  PAN,
   PER,
   PRT,
   PRY,
