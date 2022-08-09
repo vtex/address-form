489824493e6b627474c7bc8bcc3b696ec7bb8ed7
Wesley Rocha <wesleysr1997@gmail.com>
2022-08-09 13:59:28 -0400

    Trim strings on default validation

--- react/validateAddress.ts
+++ react/validateAddress.ts
@@ -283,7 +283,10 @@ function defaultValidation<FieldName extends Fields>(
 ): ValidationResult {
   const field = getField(name, rules)
 
-  if (field && !value && field.required) {
+  if (
+    field?.required &&
+    (!value || (typeof value === 'string' && !value.trim()))
+  ) {
     return emptyField
   }
 
