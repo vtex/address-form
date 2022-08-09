8348c6d1242fb18b09fe27b864cd0882bc1dbaa4
Wesley Rocha <wesleysr1997@gmail.com>
2022-08-09 15:52:25 -0400

    Add whitespace validation tests

---
+++ react/__mocks__/whitespaceAddress.ts
@@ -0,0 +1,19 @@
+import type { AddressWithValidation } from '../types/address'
+
+export default {
+  addressId: { value: '1' },
+  addressType: { value: 'residential' as const },
+  city: { value: ' ' },
+  complement: { value: ' ' },
+  country: { value: 'BRA' },
+  geoCoordinates: { value: [] },
+  neighborhood: { value: ' ' },
+  number: { value: ' ' },
+  postalCode: { value: '22231000' },
+  receiverName: { value: ' ' },
+  reference: { value: ' ' },
+  state: { value: 'RJ' },
+  street: { value: ' ' },
+  addressQuery: { value: null },
+  isDisposable: { value: true },
+} as AddressWithValidation
