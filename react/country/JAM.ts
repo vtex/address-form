01ed1b956177770375a884c110b6cf6c17df43e4
sheilagomes <sheilagomes1@gmail.com>
2023-06-21 14:55:53 -0300

    Fix GTM and JAM geolocation rules

--- react/country/JAM.ts
+++ react/country/JAM.ts
@@ -96,7 +96,7 @@ const rules: PostalCodeRules = {
     postalCode: {
       valueIn: 'long_name',
       types: ['postal_code'],
-      required: false,
+      required: true,
     },
 
     number: {
