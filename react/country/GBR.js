d2a344c5ee82045b503a94f0baa3aeb2b1312231
beatrizmaselli <beatriz.maselli@vtex.com.br>
2024-09-13 22:09:01 +0200

    update rules + shouldShowNumberKeyboard logic

--- react/country/GBR.js
+++ react/country/GBR.js
@@ -17,7 +17,8 @@ export default {
       maxLength: 50,
       fixedLabel: 'Postcode',
       required: true,
-      mask: '',
+      // UK has different patterns for postal codes alphanumericals, so we need can't use a mask.
+      mask: NaN,
       regex: /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/,
       postalCodeAPI: false,
       size: 'small',
